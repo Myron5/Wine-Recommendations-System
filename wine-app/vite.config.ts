85fea373111b27d324b4ad8fa0cdfa11ee46d6e8
Myron Melnyk <melnykmyron1808@gmail.com>
2026-07-14 22:46:43 +0200

    setup github pages

--- wine-app/vite.config.ts
+++ wine-app/vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Wine-Recommendations-System/',
   plugins: [react(), tailwindcss()],
 })
